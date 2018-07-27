be2ebad13aa7e5fd2e1ff1455255310d6ba27af1
analuizaff <nanaluff@hotmail.com>
2018-07-27 19:04:38 -0300

    Update Path.java

--- oopacman/Path.java
+++ oopacman/Path.java
@@ -5,10 +5,30 @@ public class Path implements Entity  {
   
   public Path (int x, int y){
     super(x, y, "Path");
+    setSize(10);
   }
   
   public void setCapturado(){
     capturado = true;   
   
   }
+  
+  @Override
+   public void render(GraphicsContext gc, double time) {
+          gc.setFill(new Color(1.00, 1.00, 1.00, 1.0));
+          gc.setStroke(new Color(0,0,0,1));
+          gc.fillOval(getX(), getY(), getSize(), getSize());
+          gc.strokeOval(getX(), getY(), getSize(), getSize());
+   }
+  
+   @Override
+   public void update(GraphicsContext gc, double time) {
+        if (capturado) {
+          gc.setFill(new Color(0, 0, 0, 0));
+          gc.setStroke(new Color(0,0,0,0));
+          gc.fillOval(getX(), getY(), getSize(), getSize());
+          gc.strokeOval(getX(), getY(), getSize(), getSize())
+        } else {
+   }
+  
 }
