cd7feb3041c6f79580758c44c12a236eef6f58cc
analuizaff <nanaluff@hotmail.com>
2018-07-27 19:16:46 -0300

    Update Wall.java

--- oopacman/Wall.java
+++ oopacman/Wall.java
@@ -4,6 +4,15 @@ public class Wall implements Entity{
   
   public Wall(int x, int y) {
         super(x, y, "Wall");
+        setSize(10);
   }
+  
+      @Override
+    public void render(GraphicsContext gc, double time) {
+        gc.setFill(new Color(0, 0, 1.00 , 1.00));
+        gc.setStroke(new Color(0,0,1,1));
+        gc.fillOval(getX(), getY(), getSize(), getSize());
+        gc.strokeOval(getX(), getY(), getSize(), getSize());
+}
 
 }
