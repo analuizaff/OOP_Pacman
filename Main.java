a68d567f4302c1ffb75cff0fdbcd93855db3339a
Alexylvan <37299372+Alexylvan@users.noreply.github.com>
2018-07-26 21:04:32 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import oopacman.*;
 /**
  *
@@ -12,6 +5,6 @@ import oopacman.*;
  */
 public class Main {
     public static void main(String[] args) {
-        OOPacman.main(args);
+        OOPacman.main(args); //Chama projeto principal
     }
 }
